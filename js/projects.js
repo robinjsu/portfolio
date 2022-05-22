ebfa9f064976b65db49716ea9482ccb2c2204560
Robin Su <robisu@pdx.edu>
2022-05-22 11:08:00 -0700

    use forEach loop for each project, reformat <p> elements

--- js/projects.js
+++ js/projects.js
@@ -1,11 +1,9 @@
-// import 'jquery';
-
 function fadeIn(element) {
     var fade = () => {
         $(element).animate({
             opacity: 1.0
-        })
-    }
+        });
+    };
     return fade;
 
 }
@@ -14,16 +12,16 @@ function fadeOut(element) {
     var fade = () => {
         $(element).animate({
             opacity: 0.0
-        })
-    }
+        });
+    };
     return fade;
 }
 
 $(document).ready(() => {
-    $('#proj1')
-        .hover(fadeIn('#p1-text'), fadeOut('#p1-text'))
-    // $('#p2-text')
-    //     .hover(fadeIn($(this)), fadeOut($(this)))
-    // $('#p3-text')
-    //     .hover(fadeIn($(this)), fadeOut($(this)))
-})
+    var projects = ['p1', 'p2', 'p3'];
+    projects.forEach((p) => {
+        $(`#${p}`)
+            .hover(fadeIn(`#${p}-text`), fadeOut(`#${p}-text`));
+    });
+
+});
