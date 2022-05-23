c5c9a8855edd79665e8143b01c9abd01a3ab5d7f
Robin Su <robisu@pdx.edu>
2022-05-23 16:05:36 -0700

    clean up html, add github button, animate button

--- js/projects.js
+++ js/projects.js
@@ -1,27 +1,14 @@
-function fadeIn(element) {
-    var fade = () => {
-        $(element).animate({
-            opacity: 1.0
-        });
-    };
-    return fade;
-
-}
-
-function fadeOut(element) {
-    var fade = () => {
-        $(element).animate({
-            opacity: 0.0
-        });
-    };
-    return fade;
-}
-
 $(document).ready(() => {
     var projects = ['p1', 'p2', 'p3'];
     projects.forEach((p) => {
-        $(`#${p}`)
-            .hover(fadeIn(`#${p}-text`), fadeOut(`#${p}-text`));
+        $(`#${p}`).hover(() => {
+            $(`#${p}-btn`).animate({
+                opacity: 1.0
+            });
+        }, () => {
+            $(`#${p}-btn`).animate({
+                opacity: 0.0
+            });
+        });
     });
-
 });
