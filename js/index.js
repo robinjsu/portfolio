393fe6d404288522606d37ba28f60bc416d10161
Robin Su <robisu@pdx.edu>
2022-05-24 18:27:28 -0700

    try adding smtp

---
+++ js/index.js
@@ -0,0 +1,34 @@
+const fadeBtns = () => {
+    var projects = ['p1', 'p2', 'p3'];
+    projects.forEach((p) => {
+        $(`#${p}`).hover(() => {
+            $(`#${p}-btn`).animate({
+                opacity: 1.0
+            });
+        }, () => {
+            $(`#${p}-btn`).animate({
+                opacity: 0.0
+            });
+        });
+    });
+};
+
+$(document).ready(() => {
+    fadeBtns();
+    $('.contact-form').submit((event) => {
+        Email.send({
+            Host: 'smtp.elasticemail.com',
+            Username: 'robisu@pdx.edu',
+            Password: `${SMTP_PASS}`,
+            To: 'robinjsu@gmail.com',
+            From: 'robisu@pdx.edu',
+            Subject: 'visitor message',
+            Body: `Message from ${$('#name').val()} - ${$('#email').val()}
+                   ${$('#message').val()}`
+        }).then(
+            message => alert(message)
+        );
+        event.preventDefault();
+
+    });
+});
